5ab2b4e8631005fa7cf0181643e787891845a3ba
joel.kleier <joel@kleier.us>
2016-12-20 10:00:33 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// Description:
+//   listens for trigger words to identify a user as away.
+//   when a user is mentioned while considered away, the bot mentions the user is away.
+//   when a user speaks again, they are considered no longer away.
+//
+//   inspired by the 'brb.coffee' script in the old scripts repo.
+//
+// Dependencies:
+//   none beyond hubot
+//
+// Configuration:
+//   none
+//
+// Commands:
+//   no direct commands, just listens for trigger words
+//
+// Notes:
+//   trigger words: lunch, brb, afk, bbl, bbiab, bbiaf
+//
+// Author:
+//   Joel Kleier <joel@kleier.us>
+
+module.exports = function(robot) {
+    var awayre = /\b(lunch|brb|afk|bbl|bbiab|bbiaf)\b/i;
+    var tracked = [];
+    var respond_when_away = function(msg) {
+        var username = msg.message.user.name,
+            txt = msg.message.text,
+            idx = tracked.indexOf(username),
+            is_tracked = idx >= 0,
+            is_going_away_again = awayre.test(txt),
+            numtracked,
+            metion,
+            curname;
+        if(is_tracked && !is_going_away_again) {
+            msg.send("Welcome back " + username + "!");
+            tracked.splice(idx, 1);
+        }
+        else {
+            numtracked = tracked.length;
+            for(var i = 0; i < numtracked; i++) {
+                curname = tracked[i];
+                mention = '@' + curname;
+                if(txt.indexOf(mention) >= 0) {
+                    msg.send(curname + " is currently away.");
+                    break;
+                }
+            }
+        }
+    };
+    var handle_away = function(msg) {
+        tracked.push(msg.message.user.name);
+    };
+
+    robot.hear(/./i, respond_when_away);
+    robot.hear(awayre, handle_away);
+};
